a8e1183403d0355863002fe35ac9414a4a9a436b
seo-faper <dswhdcks@gmail.com>
2022-10-06 01:47:52 +0900

    error

--- default.js
+++ default.js
@@ -998,3 +998,15 @@
 "mon_dummy_platinum.png",
 "mon_dummy_silver.png"
 ];
+const selectIndex = (totalIndex, selectingNumber) => {
+    let randomIndexArray = []
+    for (i=0; i<selectingNumber; i++) {   //check if there is any duplicate index
+      randomNum = Math.floor(Math.random() * totalIndex)
+      if (randomIndexArray.indexOf(randomNum) === -1) {
+        randomIndexArray.push(randomNum)
+      } else { //if the randomNum is already in the array retry
+        i--
+      }
+    }
+    return randomIndexArray
+};
